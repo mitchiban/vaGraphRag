c098b27bca4cdc66884045861df42e75b97fdccf
mitchiban <mitch@mitchiban.org>
2024-07-06 21:49:30 +1000

    02 tested ok

---
+++ python/02_convert_to_graph_docs.py
@@ -0,0 +1,93 @@
+import os
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_openai import AzureChatOpenAI
+
+#02
+from langchain_core.documents import Document
+from neo4j import GraphDatabase
+from langchain_community.graphs import Neo4jGraph
+from langchain_experimental.graph_transformers import LLMGraphTransformer
+from langchain_openai import ChatOpenAI
+
+
+#API
+api_key = os.getenv("AZURE_OPENAI_API_KEY")
+azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+api_version = "2024-02-15-preview"
+
+# import variables
+load_dotenv()  # Load variables from .env file
+neo4j_user = os.getenv('NEO4J_USERNAME_F')
+neo4j_password = os.getenv('NEO4J_PASSWORD_F')
+neo4j_uri = os.getenv('NEO4J_URI_F')
+
+URI = neo4j_uri
+AUTH = (neo4j_user, neo4j_password)
+
+graph = Neo4jGraph(neo4j_uri, neo4j_user, neo4j_password)
+
+
+#########################################################################################################
+#                                     Set up LLM
+#########################################################################################################
+
+
+llm = AzureChatOpenAI(
+    azure_deployment="vaGraphRAGviaaos",
+    api_version=api_version,
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+llm_transformer = LLMGraphTransformer(llm=llm)
+
+
+
+#########################################################################################################
+#                                     Exploring AuraDB: Test Harry Potter
+#########################################################################################################
+
+text = """
+Mr. and Mrs. Dursley, of number four, Privet Drive, were proud to say
+that they were perfectly normal, thank you very much. They were the last
+people you'd expect to be involved in anything strange or mysterious,
+because they just didn't hold with such nonsense.
+Mr. Dursley was the director of a firm called Grunnings, which made
+drills. He was a big, beefy man with hardly any neck, although he did
+have a very large mustache. Mrs. Dursley was thin and blonde and had
+nearly twice the usual amount of neck, which came in very useful as she
+spent so much of her time craning over garden fences, spying on the
+neighbors. The Dursleys had a small son called Dudley and in their
+opinion there was no finer boy anywhere.
+The Dursleys had everything they wanted, but they also had a secret, and
+their greatest fear was that somebody would discover it. They didn't
+think they could bear it if anyone found out about the Potters. Mrs.
+Potter was Mrs. Dursley's sister, but they hadn't met for several years;
+in fact, Mrs. Dursley pretended she didn't have a sister, because her
+sister and her good-for-nothing husband were as unDursleyish as it was
+possible to be. The Dursleys shuddered to think what the neighbors would
+say if the Potters arrived in the street. The Dursleys knew that the
+Potters had a small son, too, but they had never even seen him. This boy
+was another good reason for keeping the Potters away; they didn't want
+Dudley mixing with a child like that.
+"""
+
+# Generate triples
+documents = [Document(page_content=text)]
+graph_documents = llm_transformer.convert_to_graph_documents(documents)
+print(f"Nodes:{graph_documents[0].nodes}")
+print(f"Relationships:{graph_documents[0].relationships}")
+
+# add to graphdb
+graph.add_graph_documents(
+  graph_documents, 
+  baseEntityLabel=True, 
+  include_source=True
+)
+
+
+
